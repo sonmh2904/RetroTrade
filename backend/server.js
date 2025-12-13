076ee09d2017f21dc8f9384208fc02ee103aab87
dinhduclinh <dinhduclinh6823@gmail.com>
2025-12-13 23:32:45 +0700

    merge deloy

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,14 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: [
-      process.env.FRONTEND_URL || "http://localhost:3000",
+      process.env.FRONTEND_URL || "https://retrotrade.id.vn",
+      "https://retrotrade.vercel.app",
       "http://localhost:3000",
-      "http://127.0.0.1:3000"
+      "http://127.0.0.1:3000",
     ],
     methods: ["GET", "POST"],
-    credentials: true
-  }
+    credentials: true,
+  },
 });
 
 const connectDB = require("./src/config/db");
@@ -39,7 +40,8 @@ setIO(io);
 
 const corsOptions = {
   origin: [
-    process.env.FRONTEND_URL || "http://localhost:3000",
+    process.env.FRONTEND_URL || "https://retrotrade.id.vn",
+    "https://retrotrade.vercel.app",
     "http://localhost:3000",
     "http://127.0.0.1:3000"
   ],
